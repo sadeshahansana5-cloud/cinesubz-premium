aa1093b2cd7740f032b7d48ec31379d16c9a58b4
Sadesha Hansana <sadeshahansana5@gmail.com>
2026-08-17 13:38:50 +0530

    Add files via upload

--- api/config.js
+++ api/config.js
@@ -1,1 +1,9 @@
+const { isEmailLoginEnabled } = require('../lib/auth');
 
+module.exports = async function handler(req, res) {
+  res.status(200).json({
+    ok: true,
+    emailLoginEnabled: isEmailLoginEnabled(),
+    siteName: 'CineSubz',
+  });
+};
