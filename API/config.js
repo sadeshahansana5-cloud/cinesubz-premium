97cc2ddbe9471646d29c81818fc535f6b07b13eb
Sadesha Hansana <sadeshahansana5@gmail.com>
2026-08-17 13:37:52 +0530

    Delete API directory

--- API/config.js
+++
@@ -1,9 +0,0 @@
-const { isEmailLoginEnabled } = require('../lib/auth');
-
-module.exports = async function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    emailLoginEnabled: isEmailLoginEnabled(),
-    siteName: 'CineSubz',
-  });
-};
